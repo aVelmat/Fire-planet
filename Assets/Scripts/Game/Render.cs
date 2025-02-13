fdf0c230f8411b3a3e2e26f6343b4925e9ce0901
New-Storyline <lubomir378@gmail.com>
2025-02-14 00:11:15 +0200

    Added terrain generation sketch

--- Assets/Scripts/Game/Render.cs
+++ Assets/Scripts/Game/Render.cs
@@ -3,7 +3,36 @@ using UnityEngine;
 
 public class Render : MonoBehaviour
 {
-    internal void SetWorldState(Game.TerrainType[,] terrainMap, Building[,] buildingsMap, Unit[,] unitsMap)
+    [Header("Prefabs")]
+    public GameObject groundPrefab;
+    public GameObject mountainPrefab;
+
+    [SerializeField]
+    private Vector2 mapScale;
+
+    public void InitWorld(Game.TerrainType[,] terrainMap) {
+
+        GameObject ground = Instantiate(groundPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        ground.transform.position = new Vector3(terrainMap.GetLength(0) * mapScale.x / 2, 0, terrainMap.GetLength(1) * mapScale.y / 2);
+        ground.transform.localScale = new Vector3(terrainMap.GetLength(0) * mapScale.x, 1, terrainMap.GetLength(1) * mapScale.y);   
+
+        for (int x = 0; x < terrainMap.GetLength(0); x++)
+        {
+            for (int y = 0; y < terrainMap.GetLength(1); y++)
+            {
+                switch (terrainMap[x, y])
+                {
+                    case Game.TerrainType.mountain:
+                        Instantiate(mountainPrefab, new Vector3(x * mapScale.x, 0, y * mapScale.y), Quaternion.identity);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+    }
+    internal void SetWorldState(Building[,] buildingsMap, Unit[,] unitsMap)
     {
         throw new NotImplementedException();
     }
