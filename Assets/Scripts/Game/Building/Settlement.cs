3d27506db6db701b876a636d0b8400893f400f85
New-Storyline <lubomir378@gmail.com>
2025-02-14 13:45:32 +0200

    Updated Settelment class structure

---
+++ Assets/Scripts/Game/Building/Settlement.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+public class Settlement : Building
+{
+    internal void SetOwner(Player player)
+    {
+        throw new NotImplementedException();
+    }
+}
--- Assets/Scripts/Game/Building/SupportBuilding.cs
+++ Assets/Scripts/Game/Building/SupportBuilding.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class SupportBuilding
+public class SupportBuilding : Building
 {
     
 }
